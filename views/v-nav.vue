b9f13b198067804752cd8956bb57acb1589373da
llz <lilunze@hotmail.com>
2018-01-09 22:49:51 +0800

    添加logo栏vue组件

---
+++ views/v-nav.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="nav">
+		<div class="search">搜索</div>
+		<div class="logo">logo</div>
+		<div class="play">正在播放</div>
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style scoped>
+	
+</style>
