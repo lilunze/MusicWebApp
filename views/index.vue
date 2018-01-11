137a49542f930d15a50aa864e80dbecbaa8f76b3
llz <lilunze@hotmail.com>
2018-01-11 22:50:59 +0800

    引入每日精选组件

--- views/index.vue
+++ views/index.vue
@@ -4,6 +4,7 @@
 		<v-tag-list></v-tag-list>
 		<v-carousel></v-carousel>
 		<v-daily-recommend></v-daily-recommend>
+		<v-daily-selection></v-daily-selection>
 	</div>
 </template>
 <script>
@@ -11,13 +12,15 @@
 	import vTagList from './v-tag-list.vue';
 	import vCarousel from './v-carousel.vue';
 	import vDailyRecommend from './v-daily-recommend.vue';
+	import vDailySelection from './v-daily-selection.vue';
 	export default {
 		components:
 		{
 			vNav,
 			vTagList,
 			vCarousel,
-			vDailyRecommend
+			vDailyRecommend,
+			vDailySelection
 		}
 	}
 </script>
