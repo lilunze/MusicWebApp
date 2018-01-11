48ff9d4def3c748d9230b479dd0f7d986716e9cf
lilunze <lilunze@webadmin.com>
2018-01-11 17:50:33 +0800

    添加每日推荐数据

--- views/v-daily-recommend.vue
+++ views/v-daily-recommend.vue
@@ -1,5 +1,16 @@
 <template>
-	
+	<div>
+		<h1>独家推荐</h1>
+		<ul>
+			<li v-for="item in list">
+				<a :href="item.obj_id">
+					<p><img :src="item.sound.pic_200"></p>
+					<p>{{item.sound.name}}</p>
+					<p>{{item.sound.channel.name}}</p>
+				</a>
+			</li>
+		</ul>
+	</div>
 </template>
 <script>
 	import axios from 'axios';
@@ -11,10 +22,12 @@
 			}
 		},
 		mounted:function(){
+			var _this=this;
 			axios
 			.post('http://www.lilunze.me/api/echo/index.php',qs.stringify({'url':"http://www.app-echo.com/api/recommend/sound-day?limit=6&page=1"}))
 			.then(function(res){
-				console.log(res);
+				_this.list=res.data.list;
+				console.log(_this.list)
 			})
 			.catch(function(err){
 				console.log(err)
