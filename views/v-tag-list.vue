af7ea60ab86abbe608de79535837020870630e3e
llz <lilunze@hotmail.com>
2018-01-11 22:52:32 +0800

    完善标签列表样式

--- views/v-tag-list.vue
+++ views/v-tag-list.vue
@@ -1,13 +1,42 @@
 <template>
 	<ul class="tag-list">
-		<li v-for="item in list"><span>{{item}}</span></li>
+		<li v-for="(item,index) in list"><span :class="{'active':activeIndex==index}" @click="handleClick(index)">{{item.title}}</span></li>
 	</ul>
 </template>
 <script>
 	export default {
 		data:function(){
 			return {
-				list:['推荐','热门','原创','MV','名人']
+				activeIndex:0,
+				list:
+				[
+					{
+						title:'推荐',
+						src:''
+					},
+					{
+						title:'热门',
+						src:''
+					},
+					{
+						title:'原创',
+						src:''
+					},
+					{
+						title:'MV',
+						src:''
+					},
+					{
+						title:'名人',
+						src:''
+					}
+				]
+			}
+		},
+		methods:{
+			handleClick:function(index){
+				console.log(index)
+				this.activeIndex=index;
 			}
 		}
 	}
@@ -17,13 +46,27 @@
 	{
 		width: 100%;
 		margin: 0;
-		padding: 0;
+		box-sizing: border-box;
+		padding: 0.5rem;
 		text-align: center;
-		font-size: 1rem;
+		font-size: 0;
 	}
 	.tag-list li
 	{
 		display: inline-block;
 		width: 20%;
+		color: #4f4f4f;
+	}
+	.tag-list li span
+	{
+		display: inline-block;
+		padding: 0.2rem 0.4rem;
+		font-size: 1rem;
+		border-radius: 4px;
+	}
+	.tag-list li span.active
+	{
+		background: #dc5050;
+		color: #fff;
 	}
 </style>
