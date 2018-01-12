d5580b86a3e3e5e1d107adb4aa88305b96d1822d
lilunze <lilunze@webadmin.com>
2018-01-12 16:38:46 +0800

    更新轮播图

--- views/v-carousel.vue
+++ views/v-carousel.vue
@@ -1,20 +1,8 @@
 <template>
 	<main>
         <agile :arrows="false" :autoplay="true" :speed="1000" :fade="true" :autoplaySpeed="5000" :dots="false">
-            <div class="slide">
-                <a href="sound.html?id=1490529"><img src="https://ws-qn-echo-image-cdn.app-echo.com/Fi_-LEVfodBayt46mY-dzQAKPKPe.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100"></a>
-            </div>
-            <div class="slide">
-                <a href="sound.html?id=1488325"><img src="https://ws-qn-echo-image-cdn.app-echo.com/Ft-qRU9HJvFlfArLGg30yk9wmGtF.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100"></a>
-            </div>
-            <div class="slide">
-                <a href="sound.html?id=1479229"><img src="https://ws-qn-echo-image-cdn.app-echo.com/FvBJOVY2zDAy5ubkS_CFgaQvTeWZ.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100"></a>
-            </div>
-            <div class="slide">
-                <a href="sound.html?id=1489985"><img src="https://al-qn-echo-image-cdn.app-echo.com/Fj20s5N1mgTxkI0X10KTeHTsMWTF.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100"></a>
-            </div>
-            <div class="slide">
-                <a href="javascript:void(0)"><img src="https://ws-qn-echo-image-cdn.app-echo.com/Fi_-LEVfodBayt46mY-dzQAKPKPe.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100"></a>
+            <div class="slide" v-for="item in list">
+                <a :href="item.href"><img :src="item.src"></a>
             </div>
         </agile>
     </main>
@@ -24,17 +12,48 @@
 	import VueAgile from 'vue-agile'
 
 	Vue.use(VueAgile)
+
+    export default {
+        data:function(){
+            return {
+                list:
+                [
+                    {
+                        href:'/sound/:id=1507279',
+                        src:'https://qn-qn-echo-image-cdn.app-echo.com/FiTasCFgLLsYpr1j8ahuHkXKNr6B.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100'
+                    },
+                    {
+                        href:'/sound/:id=1508864',
+                        src:'https://qn-qn-echo-image-cdn.app-echo.com/Fixe6DFO0JcLg5zIyKCGSj8noKqp.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100'
+                    },
+                    {
+                        href:'/sound/:id=1508513',
+                        src:'https://qn-qn-echo-image-cdn.app-echo.com/FiEW4fLo6g2wPDML4v5NwOHH43fJ.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100'
+                    },
+                    {
+                        href:'/sound/:id=1489985',
+                        src:'https://al-qn-echo-image-cdn.app-echo.com/Fj20s5N1mgTxkI0X10KTeHTsMWTF.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100'
+                    },
+                    {
+                        href:'/sound/:id=15007747',
+                        src:'https://qn-qn-echo-image-cdn.app-echo.com/FgLvBO2yBxKdJXQpF0MHH-B429iO.jpg?imageMogr2/auto-orient/quality/100%7CimageView2/4/w/640/q/100'
+                    }
+                ]
+            }
+        }
+    }
 </script>
 <style scoped>
 	.slide 
 	{
 	    position: relative;
+        background: #fff;
 	}
 	.slide a
 	{
         left: 50%;
         margin: 0;
-        /*position: absolute;*/
+        font-size: 0;
         top: 50%;
         transform: translate(-50%, -50%);
     }
