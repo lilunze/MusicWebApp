04d029baaf5b6fdf50ff3f7cc969a6259ed929d7
lilunze <lilunze@webadmin.com>
2018-01-16 15:49:44 +0800

    将logo栏、标签导航栏添加到全局组件

--- app.vue
+++ app.vue
@@ -1,10 +1,18 @@
 <template>
 	<div>
+		<v-nav></v-nav>
+		<v-tag-list></v-tag-list>
 		<router-view></router-view>
 	</div>
 </template>
 <script>
+	import vNav from './views/v-nav.vue';
+	import vTagList from './views/v-tag-list.vue';
 	export default {
-		
+		components:
+		{
+			vNav,
+			vTagList
+		}
 	}
 </script>
