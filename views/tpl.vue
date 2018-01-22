1f6ebc1581c862890f5dc9dec4b7943db5420f99
lilunze <lilunze@webadmin.com>
2018-01-22 17:53:33 +0800

    新建头部slot分发模式

---
+++ views/tpl.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<v-nav></v-nav>
+		<v-tag-list></v-tag-list>
+		<slot></slot>
+	</div>
+</template>
+<script>
+	import vNav from './v-nav.vue';
+	import vTagList from './v-tag-list.vue';
+	import vCarousel from './v-carousel.vue';
+	import vSublist from './v-sublist.vue';
+	import vDailyRecommend from './v-daily-recommend.vue';
+	import vDailySelection from './v-daily-selection.vue';
+	export default {
+		components:
+		{
+			vNav,
+			vTagList,
+			vCarousel,
+			vSublist,
+			vDailyRecommend,
+			vDailySelection
+		}
+	}
+</script>
+<style scoped>
+	
+</style>
