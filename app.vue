f1db414fc3c8bbb4c50fdfc1bf4edc08e60b4ee3
lilunze <lilunze@webadmin.com>
2018-01-22 17:50:57 +0800

    删除头部模版

--- app.vue
+++ app.vue
@@ -1,18 +1,8 @@
 <template>
-	<div>
-		<v-nav></v-nav>
-		<v-tag-list></v-tag-list>
-		<router-view></router-view>
-	</div>
+	<router-view></router-view>
 </template>
 <script>
-	import vNav from './views/v-nav.vue';
-	import vTagList from './views/v-tag-list.vue';
 	export default {
-		components:
-		{
-			vNav,
-			vTagList
-		}
+		
 	}
 </script>
